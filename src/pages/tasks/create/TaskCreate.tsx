a377a57268ad5e2177b20b1e299a6c301c67faf3
Matthew Fedin <plumpalbert@gmail.com>
2025-03-15 11:49:03 +0300

    implemented simple form to create task

---
+++ src/pages/tasks/create/TaskCreate.tsx
@@ -0,0 +1,99 @@
+import { FC, useCallback } from 'react';
+import { Button, Card, Form, Input, Radio } from 'antd';
+import { DifficultyValues, EDifficulty, TaskSchema } from './types';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const FormSchema = TaskSchema.omit({ id: true });
+
+export const TaskCreatePage: FC = () => {
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: '',
+      difficulty: EDifficulty.Medium,
+    },
+  });
+
+  const submitHandler = useCallback<SubmitHandler<z.infer<typeof FormSchema>>>(
+    (values) => {
+      console.debug('=> values:', values);
+    },
+    []
+  );
+
+  return (
+    <Card title="New task">
+      <Form
+        layout="vertical"
+        onFinish={form.handleSubmit(submitHandler)}
+      >
+        <Form.Item label="Title">
+          <Controller
+            control={form.control}
+            name="title"
+            render={({ field, fieldState }) => (
+              <Input
+                {...field}
+                status={fieldState.error ? 'error' : undefined}
+              />
+            )}
+          />
+        </Form.Item>
+
+        <Form.Item label="Difficulty">
+          <Controller
+            control={form.control}
+            name="difficulty"
+            render={({ field }) => (
+              <Radio.Group
+                {...field}
+                buttonStyle="solid"
+              >
+                {DifficultyValues.map((value) => (
+                  <Radio.Button
+                    key={value}
+                    value={value}
+                  >
+                    {EDifficulty[value]}
+                  </Radio.Button>
+                ))}
+              </Radio.Group>
+            )}
+          />
+        </Form.Item>
+
+        <Form.Item label="Description">
+          <Controller
+            control={form.control}
+            name="description"
+            render={({ field, fieldState }) => (
+              <Input.TextArea
+                {...field}
+                rows={6}
+                status={fieldState.error ? 'error' : undefined}
+              />
+            )}
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            block
+            htmlType="submit"
+            type="primary"
+            disabled={
+              form.formState.disabled ||
+              form.formState.isValidating ||
+              form.formState.isSubmitting ||
+              !form.formState.isValid
+            }
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
