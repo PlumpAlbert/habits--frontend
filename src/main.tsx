778e4b5a69ade6c0d66880db251b26b154acae29
Matthew Fedin <plumpalbert@gmail.com>
2025-03-15 12:49:08 +0300

    removed unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 
