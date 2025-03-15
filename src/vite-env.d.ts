bc1e3f034b454254715e3c685263714d380717ba
Matthew Fedin <plumpalbert@gmail.com>
2025-03-15 12:58:12 +0300

    added type definition for API endpoint

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** API endpoint */
+  readonly VITE_API: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
